78b491e4bf62ba13c849f23a49ff5ab88f8f229d
Adn <adnmaulidya>
2019-12-11 22:52:31 +0700

    Selesai

--- app/src/main/java/com/example/pushnotif/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/pushnotif/MyFirebaseMessagingService.java
@@ -46,14 +46,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 PendingIntent.FLAG_ONE_SHOT);
 
         Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
-                R.drawable.ic_launcher_background);
+                R.drawable.daisy);
 
         Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setSmallIcon(R.drawable.daisy)
                 .setLargeIcon(largeIcon)
                 .setContentTitle(remoteMessage.getData().get("title"))
                 .setContentText(remoteMessage.getData().get("message"))
+                .setStyle(new NotificationCompat.BigTextStyle())
                 .setAutoCancel(true)
                 .setSound(notificationSoundUri)
                 .setContentIntent(pendingIntent);
